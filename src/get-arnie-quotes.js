9b25588cb22d38b2d509487705ea66a9ff1d1a79
cronullarc <ryancronulla@gmail.com>
2023-11-10 10:47:42 +1100

    Finalize Smokeball Challenge

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,22 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = await Promise.all(
+    urls.map(async (url) => {
+      try {
+        const response = await httpGet(url);
+        const message = JSON.parse(response.body).message;
+        return response.status === 200 ? { 'Arnie Quote': message } : { 'FAILURE': message };
+      } catch (error) {
+        return { 'FAILURE': error.message };
+      }
+    })
+  );
+  return results;
 };
+
 
 module.exports = {
   getArnieQuotes,
 };
+
